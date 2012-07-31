1a9f0453545e60c69cd9c67a94d1c094b4bbbb5e
Von Welch <vwelch@indiana.edu>
2012-07-31 04:12:26 +0000

    Updated tests_cert_retrieve.py:test_help() to work
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/trunk@15278 4e558342-562e-0410-864c-e07659590f8c

--- tests/tests_cert_retrieve.py
+++ tests/tests_cert_retrieve.py
@@ -4,11 +4,10 @@ import PKIClientTestCase
 
 class CertRetrieveTests(PKIClientTestCase.PKIClientTestCase):
 
-    # TODO: Should be named cert-retrieve-new to make DOE Grids scripts
-    command = "RetrieveCert.py"
+    command = "cert-retrieve-new.py"
 
     def test_help(self):
         """Test running with -h to get help"""
         env = self.get_TestFileEnvironment()
-        result = env.run(self.command, "-h")
-        self.assertIn("usage:", result.stdout)
+        result = env.run("python", self.command, "-h")
+        self.assertIn("Usage:", result.stdout)
