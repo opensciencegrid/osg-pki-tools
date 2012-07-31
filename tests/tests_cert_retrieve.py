6370d08030d2575a1123663cd08b233af778c052
Von Welch <vwelch@indiana.edu>
2012-07-31 04:23:48 +0000

    test_help(): print useful error message on failure
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/trunk@15281 4e558342-562e-0410-864c-e07659590f8c

--- tests/tests_cert_retrieve.py
+++ tests/tests_cert_retrieve.py
@@ -9,4 +9,5 @@ class CertRetrieveTests(PKIClientTestCase.PKIClientTestCase):
     def test_help(self):
         """Test running with -h to get help"""
         result = self.run_script(self.command, "-h")
-        self.assertTrue("Usage:" in result.stdout)
+        self.assertTrue("Usage:" in result.stdout,
+                        self.run_error_msg(result))
