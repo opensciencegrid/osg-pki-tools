034862241fc8cb9850c346f2d1e2a82aaea472a4
Von Welch <vwelch@indiana.edu>
2012-07-31 02:24:50 +0000

    Use optparse instead of argparse and import unittest2 to work with python<2.7
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/trunk@15275 4e558342-562e-0410-864c-e07659590f8c

--- tests/do-tests.py
+++ tests/do-tests.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 """Run tests on OSG PKI Command Line client scripts"""
 
-import argparse
 import getpass
+import optparse
 import os
 import os.path
 import subprocess
 import sys
-import unittest
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 from PKIClientTestCase import PKIClientTestCase
 
@@ -19,43 +22,37 @@ def main(argv=None):
 	argv = sys.argv
 
     # Argument parsing
-    parser = argparse.ArgumentParser(
-	description=__doc__, # printed with -h/--help
-	# Don't mess with format of description
-	formatter_class=argparse.ArgumentDefaultsHelpFormatter
-	# To have --help print defaults with trade-off it changes
-	# formatting, use: 
-	)
-    parser.add_argument("-c", "--certificate",
-			default=PKIClientTestCase.get_user_cert_path(),
-			help="Specific certificate for authentication",
-			metavar="PATH")
-    parser.add_argument("-p", "--privatekey",
-			default=PKIClientTestCase.get_user_key_path(),
-			help="Specific private key for authentication",
-			metavar="PATH")
-    parser.add_argument("-P", "--nopassphrase",
-                        default=False,
-                        action="store_const", const=True,
-                        help="Do not prompt for pass phrase")
-    parser.add_argument("-T", "--tests",
-                        default="tests_*.py",
-                        help="Specify tests to run",
-                        metavar="GLOB")
-    parser.add_argument("-v", "--verbose",
-                        default=1,
-                        action="store_const", const=2,
-                        help="Run tests verbosely")
+    parser = optparse.OptionParser()
+    parser.add_option("-c", "--certificate",
+                      default=PKIClientTestCase.get_user_cert_path(),
+                      help="Specific certificate for authentication",
+                      metavar="PATH")
+    parser.add_option("-p", "--privatekey",
+                      default=PKIClientTestCase.get_user_key_path(),
+                      help="Specific private key for authentication",
+                      metavar="PATH")
+    parser.add_option("-P", "--nopassphrase",
+                      default=False,
+                      action="store_const", const=True,
+                      help="Do not prompt for pass phrase")
+    parser.add_option("-T", "--tests",
+                      default="tests_*.py",
+                      help="Specify tests to run",
+                      metavar="GLOB")
+    parser.add_option("-v", "--verbose",
+                      default=1,
+                      action="store_const", const=2,
+                      help="Run tests verbosely")
 
-    args = parser.parse_args()
+    (options, args) = parser.parse_args()
 
     # Store parameters so test cases can access them...
-    PKIClientTestCase.set_user_cert_path(args.certificate)
+    PKIClientTestCase.set_user_cert_path(options.certificate)
     print "User certificate: " + PKIClientTestCase.get_user_cert_path()
-    PKIClientTestCase.set_user_key_path(args.privatekey)
+    PKIClientTestCase.set_user_key_path(options.privatekey)
     print "User private key: " + PKIClientTestCase.get_user_key_path()
 
-    if args.nopassphrase:
+    if options.nopassphrase:
         pass_phrase = None
     else:
         pass_phrase = getpass.getpass(
@@ -63,12 +60,12 @@ def main(argv=None):
     PKIClientTestCase.set_user_key_pass_phrase(pass_phrase)
 
     loader = unittest.TestLoader()
-    print "Running tests in " + args.tests
-    suite = loader.discover(".", pattern=args.tests)
+    print "Running tests in " + options.tests
+    suite = loader.discover(".", pattern=options.tests)
 
     #
     # Do it...
-    runner = unittest.runner.TextTestRunner(verbosity=args.verbose)
+    runner = unittest.runner.TextTestRunner(verbosity=options.verbose)
     print "Running tests:"
     runner.run(suite)
 
