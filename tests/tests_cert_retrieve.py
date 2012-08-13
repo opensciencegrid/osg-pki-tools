4ed803177354379b7e6463bac4cc5ad344066e6d
Von Welch <vwelch@indiana.edu>
2012-08-13 20:47:26 +0000

    Added test_retrieve()
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/trunk@15388 4e558342-562e-0410-864c-e07659590f8c

--- tests/tests_cert_retrieve.py
+++ tests/tests_cert_retrieve.py
@@ -11,3 +11,11 @@ class CertRetrieveTests(PKIClientTestCase.PKIClientTestCase):
         result = self.run_script(self.command, "-h")
         self.assertTrue("Usage:" in result.stdout,
                         self.run_error_msg(result))
+
+    def test_retrieve(self):
+        """Test retrieving a certificate"""
+        # 90 is a known good certificate but otherwise arbitrary
+        result = self.run_script(self.command, "-i", "90")
+        err_msg = self.run_error_msg(result)
+        self.assertEqual(result.returncode, 0, err_msg)
+        self.assertTrue("Certificate written to" in result.stdout, err_msg)
