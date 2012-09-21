479c97d3145745e8fe51aab972d1188e57fd6611
Von Welch <vwelch@indiana.edu>
2012-09-21 19:44:46 +0000

    setup.py: Changed version to 1.0
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/tags/1.0@15571 4e558342-562e-0410-864c-e07659590f8c

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "OSGPKITools",
-    version = "0.0.1",
+    version = "1.0",
     author = "Rohan Mathure",
     author_email = "rmathure@indiana.edu",
     description = ("A utility to request and manage certificates."),
