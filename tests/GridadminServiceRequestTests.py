28624751dbf2c07d6faaf6eadf77a3cc38c27a9c
Rohan Mathure <rmathure@umail.iu.edu>
2012-10-21 14:09:29 +0000

    Merging branch 1.0 to trunk
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/trunk@15734 4e558342-562e-0410-864c-e07659590f8c

---
+++ tests/GridadminServiceRequestTests.py
@@ -0,0 +1,52 @@
+"""Test osg-gridadmin-cert-request script"""
+
+import os.path
+import re
+
+import PKIClientTestCase
+
+class GridadminServiceRequestTests(PKIClientTestCase.PKIClientTestCase):
+
+    command = "osg-gridadmin-cert-request"
+
+    def test_single_host_request(self):
+        """Test making a request for a single service credential"""
+        env = self.get_test_env()
+        fqdn = "test/test." + self.domain
+        result = self.run_script(env,
+                                 self.command,
+                                 "-H", fqdn,
+                                 "-k", self.get_key_path(),
+                                 "-c", self.get_cert_path())
+        err_msg = self.run_error_msg(result)
+        self.assertEqual(result.returncode, 0, err_msg)
+        match = re.search("^Id is: (\d+)\s*$",
+                          result.stdout,
+                          re.MULTILINE)
+        self.assertNotEqual(match, None,
+                            "Could not find request Id: " + err_msg)
+        id = int(match.group(1))
+        match = re.search("Certificate written to (.*)$",
+                          result.stdout,
+                          re.MULTILINE)
+        self.assertNotEqual(match, None,
+                            "Could not find output certificate: " + err_msg)
+        cert_file = match.group(1)
+        # Check output certificate
+        cert_result = self.check_certificate(env, cert_file)
+        err_msg = self.run_error_msg(result)
+        self.assertEqual(result.returncode, 0,
+                         "Failed checking certificate %s: %s" % (cert_file,
+                                                                 err_msg))
+        # Check output key
+        key_file = fqdn.replace('/','-') + "-key.pem"
+        self.assertTrue(result.files_created.has_key(key_file))
+        key_result = self.check_private_key(env, key_file)
+        err_msg = self.run_error_msg(key_result)
+        self.assertEqual(result.returncode, 0,
+                         "Check of private key %s failed: %s" % (key_file,
+                                                                 err_msg))
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
---
+++ tests/do-tests.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""Run tests on OSG PKI Command Line client scripts"""
+
+import getpass
+import optparse
+import os
+import os.path
+import subprocess
+import sys
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from PKIClientTestCase import PKIClientTestCase
+
+
+def main(argv=None):
+    # Do argv default this way, as doing it in the functional
+    # declaration sets it at compile time.
+    if argv is None:
+	argv = sys.argv
+
+    # Argument parsing
+    parser = optparse.OptionParser()
+    parser.add_option("-c", "--certificate",
+                      default=PKIClientTestCase.get_cert_path(),
+                      help="Specify certificate for authentication",
+                      metavar="PATH")
+    parser.add_option("-p", "--privatekey",
+                      default=PKIClientTestCase.get_key_path(),
+                      help="Specify private key for authentication",
+                      metavar="PATH")
+    parser.add_option("-s", "--scripts-path",
+                      default="../osgpkitools",
+                      help="Specify path to scripts",
+                      metavar="PATH")
+    parser.add_option("-T", "--tests",
+                      default="*Tests.py",
+                      help="Specify tests to run",
+                      metavar="GLOB")
+    parser.add_option("-v", "--verbose",
+                      default=1,
+                      action="store_const", const=2,
+                      help="Run tests verbosely")
+
+    (options, args) = parser.parse_args()
+
+    # Store parameters so test cases can access them...
+    PKIClientTestCase.set_cert_path(options.certificate)
+    print "Test certificate: " + PKIClientTestCase.get_cert_path()
+    PKIClientTestCase.set_key_path(options.privatekey)
+    print "Test private key: " + PKIClientTestCase.get_key_path()
+    PKIClientTestCase.set_scripts_path(options.scripts_path)
+    print "Path to scripts: " + PKIClientTestCase.get_scripts_path()
+
+    loader = unittest.TestLoader()
+    print "Running tests in " + options.tests
+    suite = loader.discover(".", pattern=options.tests)
+
+    #
+    # Do it...
+    runner = unittest.runner.TextTestRunner(verbosity=options.verbose)
+    print "Running tests, this may take a while:"
+    runner.run(suite)
+
+    return(0)
+
+if __name__ == "__main__":
+    sys.exit(main())
