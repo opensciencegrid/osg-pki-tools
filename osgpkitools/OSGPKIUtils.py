f8eddbe91f6f14a14a18433782126b768eb4a346
Rohan Mathure <rmathure@umail.iu.edu>
2012-12-25 16:29:50 +0000

    pep8 standardizing as per JIRA#289
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/trunk@16034 4e558342-562e-0410-864c-e07659590f8c

--- osgpkitools/OSGPKIUtils.py
+++ osgpkitools/OSGPKIUtils.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 #
-#vim: ts=4 sw=4 nowrap
+# vim: ts=4 sw=4 nowrap
 #
 
 import M2Crypto
@@ -14,96 +16,119 @@ import textwrap
 from ExceptionDefinitions import *
 
 # These flags are for the purpose of passing to the M2Crypto calls and are used later in the script
+
 MBSTRING_FLAG = 0x1000
-MBSTRING_ASC  = MBSTRING_FLAG | 1
-MBSTRING_BMP  = MBSTRING_FLAG | 2
+MBSTRING_ASC = MBSTRING_FLAG | 1
+MBSTRING_BMP = MBSTRING_FLAG | 2
+
 
 def check_response_500(response):
     """ This functions handles the 500 error response from the server"""
-    if response.status==500:
-        raise Exception_500response(response.status, reponse.reason)
-	#charlimit_textwrap("Request Failed. Status %s" %response.status)
-        #charlimit_textwrap("Reason for failure %s" %response.reason)
-        #sys.exit(1)
 
+    if response.status == 500:
+        raise Exception_500response(response.status, reponse.reason)
+
+
+    # charlimit_textwrap("Request Failed. Status %s" %response.status)
+        # charlimit_textwrap("Reason for failure %s" %response.reason)
+        # sys.exit(1)
 
 def check_failed_response(data):
     """ This function checks if the response is failed"""
+
     if 'FAILED' in data:
         print_failure_reason_exit(data)
 
+
 def print_failure_reason_exit(data):
     """This functions prints the failure reasons and exits"""
+
     try:
-        charlimit_textwrap('The request has failed for the following reason:\n%s' \
-            % simplejson.loads(data)['detail'].split('--')[1].lstrip())
+        charlimit_textwrap('The request has failed for the following reason:\n%s'
+                            % simplejson.loads(data)['detail'
+                           ].split('--')[1].lstrip())
     except IndexError, e:
-        charlimit_textwrap('The request has failed for the following reason:\n%s' \
-            % simplejson.loads(data)['detail'].lstrip())
-        charlimit_textwrap('Status : %s ' % simplejson.loads(data)['status'])
+        charlimit_textwrap('The request has failed for the following reason:\n%s'
+                            % simplejson.loads(data)['detail'].lstrip())
+        charlimit_textwrap('Status : %s '
+                           % simplejson.loads(data)['status'])
     sys.exit(1)
 
+
 def check_for_pending(data, iterations, **arguments):
-        """ This function is a centralized location to print the in process output indication"""
-	time.sleep(5)
-	iterations = iterations + 1
-	if iterations % 6 == 0:
-	    print '.',
-	    sys.stdout.flush()
-	check_timeout(iterations, arguments['timeout'])
-	#if iterations > arguments['timeout']*12:
-	#    raise TimeoutException(arguments['timeout'])
-	#    #charlimit_textwrap('Timeout reached in %s minutes. This script will now exit.' % arguments['timeout'] )
-	    #charlimit_textwrap(' You can open goc ticket to track this issue by going to https://ticket.grid.iu.edu\n')
-	    #sys.exit(1)
-	return iterations
+    """ This function is a centralized location to print the in process output indication"""
+
+    time.sleep(5)
+    iterations = iterations + 1
+    if iterations % 6 == 0:
+        print '.',
+        sys.stdout.flush()
+    check_timeout(iterations, arguments['timeout'])
+
+    # if iterations > arguments['timeout']*12:
+    #    raise TimeoutException(arguments['timeout'])
+    #    #charlimit_textwrap('Timeout reached in %s minutes. This script will now exit.' % arguments['timeout'] )
+        # charlimit_textwrap(' You can open goc ticket to track this issue by going to https://ticket.grid.iu.edu\n')
+        # sys.exit(1)
+
+    return iterations
+
 
 def check_timeout(iterations, timeout):
-	"""This function checks for a timeout. If timeout has occurred it raises a TIMEOUT EXCEPTION"""
-	if iterations > timeout*12:
-	    raise TimeoutException(timeout)
-	else:
-	    return
-	
+    """This function checks for a timeout. If timeout has occurred it raises a TIMEOUT EXCEPTION"""
+
+    if iterations > timeout * 12:
+        raise TimeoutException(timeout)
+    else:
+        return
+
+
 def charlimit_textwrap(string):
     """This function wraps up tht output to 80 characters. Accepts string and print the wrapped output"""
+
     list_string = textwrap.wrap(string)
     for line in list_string:
         print line
     return
 
+
 def get_request_count(filename):
-	'''Returns the number of hostname requested in the file supplied as -f furing bulk certificate request'''
-	hostfile = open(filename, 'rb')
-	name_set = set()
-	count = 0
-	for line in hostfile.readlines():
-		line = line.strip(' \n')
-		if not line in name_set:
-			name_set.add(line)
-			count +=1
-	hostfile.close()
-	return count
+    '''Returns the number of hostname requested in the file supplied as -f furing bulk certificate request'''
+
+    hostfile = open(filename, 'rb')
+    name_set = set()
+    count = 0
+    for line in hostfile.readlines():
+        line = line.strip(' \n')
+        if not line in name_set:
+            name_set.add(line)
+            count += 1
+    hostfile.close()
+    return count
+
 
 ### We take the whole certificate data as a stirng input
 ### Checking for /CN= in every line and extracting the term after that if not Digicert i.e. CA would be the hostname
 ### Here we rely on OPenSSL -printcert output format. If it changes our output might be affected
 
 def extractHostname(certString):
-	"""Extracts hostname from the string of certifcate file
-	We take the whole certificate data as a stirng input
-	Checking for /CN= in every line and extracting the term after that if not Digicert i.e. CA would be the hostname
-	Here we rely on OPenSSL -printcert output format. If it changes our output might be affected"""
-	certArray = certString.split(' ')
-	hostname = ""
-	for subStr in certArray:
-		if '/CN=' in subStr:
-			if not 'DigiCert' in subStr.split('/CN=')[1].split('\n')[0]:
-				hostname = subStr.split('/CN=')[1].split('\n')[0]
-			
-	if hostname == "":
-		raise UnexpectedBehaviourException("Unexpected behaviour by OIM retrive API. EEC certificate not found")
-	return hostname
+    """Extracts hostname from the string of certifcate file
+....We take the whole certificate data as a stirng input
+....Checking for /CN= in every line and extracting the term after that if not Digicert i.e. CA would be the hostname
+....Here we rely on OPenSSL -printcert output format. If it changes our output might be affected"""
+
+    certArray = certString.split(' ')
+    hostname = ''
+    for subStr in certArray:
+        if '/CN=' in subStr:
+            if not 'DigiCert' in subStr.split('/CN=')[1].split('\n')[0]:
+                hostname = subStr.split('/CN=')[1].split('\n')[0]
+
+    if hostname == '':
+        raise UnexpectedBehaviourException('Unexpected behaviour by OIM retrive API. EEC certificate not found'
+                )
+    return hostname
+
 
 ### Checking for a blank new line to seperate the certificates
 ### Then cheking if the hotsname is present in the certificate chunk
@@ -111,92 +136,117 @@ def extractHostname(certString):
 ### Here we rely on OPenSSL -printcert output format. If it changes our output might be affected
 
 def extractEEC(certString, hostname):
-	certArray = certString.split('\n\n')
-	for certArrayString in certArray: 
-		if (hostname in certArrayString):
-			return certArrayString
+    certArray = certString.split('''
 
-		
-def CreateOIMConfig (isITB, **OIMConfig):
-	Config = ConfigParser.ConfigParser()
-	if os.path.exists(str(os.environ['HOME'])+'/.osg-pki/OSG_PKI.ini'):
-		print "Overriding INI file with %s/.osg-pki/OSG_PKI.ini" %str(os.environ['HOME'])
-		Config.read(str(os.environ['HOME'])+'/.osg-pki/OSG_PKI.ini')
-	elif os.path.exists('pki-clients.ini'):
-		Config.read('pki-clients.ini')
-	elif os.path.exists('/etc/pki-clients.ini'):
-		Config.read('/etc/pki-clients.ini')
-	else:
-		raise FileNotFoundException('pki-clients.ini', 'Could not locate the file')
-	if isITB:
-		print "Running in test mode"
-		OIM = 'OIMData_ITB'
-		OIMConfig.update({'host': 'oim-itb.grid.iu.edu:80'})
-		OIMConfig.update({'hostsec': 'oim-itb.grid.iu.edu:443'})
-	else:
-		OIM = 'OIMData'
-		OIMConfig.update({'host': 'oim.grid.iu.edu:80'})
-		OIMConfig.update({'hostsec': 'oim.grid.iu.edu:443'})
-	OIMConfig.update({'requrl': Config.get(OIM, 'requrl')})
-	OIMConfig.update({'appurl': Config.get(OIM, 'appurl')})
-	OIMConfig.update({'revurl': Config.get(OIM, 'revurl')})
-	OIMConfig.update({'canurl': Config.get(OIM, 'canurl')})
-	OIMConfig.update({'returl': Config.get(OIM, 'returl')})
-	OIMConfig.update({'issurl': Config.get(OIM, 'issurl')})
-	OIMConfig.update({'quotaurl': Config.get(OIM, 'quotaurl')})
-	OIMConfig.update({'content_type': Config.get(OIM, 'content_type')})
-	return OIMConfig
+''')
+    for certArrayString in certArray:
+        if hostname in certArrayString:
+            return certArrayString
+
+
+def CreateOIMConfig(isITB, **OIMConfig):
+    Config = ConfigParser.ConfigParser()
+    if os.path.exists(str(os.environ['HOME']) + '/.osg-pki/OSG_PKI.ini'
+                      ):
+        print 'Overriding INI file with %s/.osg-pki/OSG_PKI.ini' \
+            % str(os.environ['HOME'])
+        Config.read(str(os.environ['HOME']) + '/.osg-pki/OSG_PKI.ini')
+    elif os.path.exists('pki-clients.ini'):
+        Config.read('pki-clients.ini')
+    elif os.path.exists('/etc/pki-clients.ini'):
+        Config.read('/etc/pki-clients.ini')
+    else:
+        raise FileNotFoundException('pki-clients.ini',
+                                    'Could not locate the file')
+    if isITB:
+        print 'Running in test mode'
+        OIM = 'OIMData_ITB'
+        OIMConfig.update({'host': 'oim-itb.grid.iu.edu:80'})
+        OIMConfig.update({'hostsec': 'oim-itb.grid.iu.edu:443'})
+    else:
+        OIM = 'OIMData'
+        OIMConfig.update({'host': 'oim.grid.iu.edu:80'})
+        OIMConfig.update({'hostsec': 'oim.grid.iu.edu:443'})
+    OIMConfig.update({'requrl': Config.get(OIM, 'requrl')})
+    OIMConfig.update({'appurl': Config.get(OIM, 'appurl')})
+    OIMConfig.update({'revurl': Config.get(OIM, 'revurl')})
+    OIMConfig.update({'canurl': Config.get(OIM, 'canurl')})
+    OIMConfig.update({'returl': Config.get(OIM, 'returl')})
+    OIMConfig.update({'issurl': Config.get(OIM, 'issurl')})
+    OIMConfig.update({'quotaurl': Config.get(OIM, 'quotaurl')})
+    OIMConfig.update({'content_type': Config.get(OIM, 'content_type')})
+    return OIMConfig
+
 
 class Cert:
-	def __init__ ( self ):
-		self.RsaKey = { 'KeyLength'       : 2048,
-						'PubExponent'     : 0x10001,		# -> 65537
-						'keygen_callback' : self.callback 
-				  }
 
-		self.KeyPair         = None
-		self.PKey            = None
+    def __init__(self):
+        self.RsaKey = {'KeyLength': 2048, 'PubExponent': 0x10001,
+                       'keygen_callback': self.callback}  # -> 65537
 
-		self.X509Request     = None 
-		self.X509Certificate = None
+        self.KeyPair = None
+        self.PKey = None
 
-	def callback ( self, *args ):
-		return None
+        self.X509Request = None
+        self.X509Certificate = None
+
+    def callback(self, *args):
+        return None
+
+    def CreatePKey(self, filename):
+        """This function accepts the filename of the key file to write to.
+........It write the private key to the specified file name without ciphering it."""
+
+        self.KeyPair = M2Crypto.RSA.gen_key(self.RsaKey['KeyLength'],
+                self.RsaKey['PubExponent'],
+                self.RsaKey['keygen_callback'])
+        PubKey = M2Crypto.RSA.new_pub_key(self.KeyPair.pub())
+        self.KeyPair.save_key(filename, cipher=None)
+        self.PKey = M2Crypto.EVP.PKey(md='sha1')
+        self.PKey.assign_rsa(self.KeyPair)
+
+    def CreateX509Request(self, **config_items):
+        """This function accepts a dctionary that contains information regarding the CSR.
+........It creates a CSR and returns it to the calling script."""
+
+        #
+        # X509 REQUEST
+        #
+
+        self.X509Request = M2Crypto.X509.Request()
+
+        #
+        # subject
+        #
+
+        X509Name = M2Crypto.X509.X509_Name()
+
+        X509Name.add_entry_by_txt(  # common name
+            field='CN',
+            type=MBSTRING_ASC,
+            entry=config_items['CN'],
+            len=-1,
+            loc=-1,
+            set=0,
+            )
+        if config_items.has_key('emailAddress'):
+            X509Name.add_entry_by_txt(  # pkcs9 email address
+                field='emailAddress',
+                type=MBSTRING_ASC,
+                entry=config_items['emailAddress'],
+                len=-1,
+                loc=-1,
+                set=0,
+                )
+
+        self.X509Request.set_subject_name(X509Name)
+
+        #
+        # publickey
+        #
+
+        self.X509Request.set_pubkey(pkey=self.PKey)
+        self.X509Request.sign(pkey=self.PKey, md='sha1')
+        return self.X509Request
 
 
-	def CreatePKey ( self , filename ):
-		"""This function accepts the filename of the key file to write to.
-		It write the private key to the specified file name without ciphering it."""
-		self.KeyPair = M2Crypto.RSA.gen_key( self.RsaKey['KeyLength'], self.RsaKey['PubExponent'], self.RsaKey['keygen_callback'] )
-		PubKey = M2Crypto.RSA.new_pub_key( self.KeyPair.pub () )
-		self.KeyPair.save_key( filename, cipher=None)
-		self.PKey = M2Crypto.EVP.PKey ( md='sha1')
-		self.PKey.assign_rsa ( self.KeyPair )
-
-
-	def CreateX509Request ( self, **config_items ):
-		"""This function accepts a dctionary that contains information regarding the CSR.
-		It creates a CSR and returns it to the calling script."""
-		#
-		# X509 REQUEST
-		#
-
-		self.X509Request = M2Crypto.X509.Request ()
-
-		#
-		# subject
-		#
-
-		X509Name = M2Crypto.X509.X509_Name ()
-
-		X509Name.add_entry_by_txt ( field='CN',           type=MBSTRING_ASC, entry=config_items['CN'],    len=-1, loc=-1, set=0 )    # common name
-		if config_items.has_key('emailAddress'):
-			X509Name.add_entry_by_txt ( field='emailAddress', type=MBSTRING_ASC, entry=config_items['emailAddress'],        len=-1, loc=-1, set=0 )    # pkcs9 email address     
-
-		self.X509Request.set_subject_name( X509Name )
-		#
-		# publickey
-		#
-		self.X509Request.set_pubkey ( pkey=self.PKey )
-		self.X509Request.sign ( pkey=self.PKey, md = 'sha1')
-		return self.X509Request
