a9d7f5e3bedccd600a91c438811bf9cf3f7d3cbf
Von Welch <vwelch@indiana.edu>
2013-01-23 00:47:33 +0000

    Basic working version of BaTLab tests.
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/trunk@16165 4e558342-562e-0410-864c-e07659590f8c

--- tests/batlab/run-tests.sh
+++ tests/batlab/run-tests.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
+# Eventual return status, will be set to non-zero on error
+status=0
+
 echo "Running BaTLab tests"
 /bin/date
+uname -a
+svn info
 
-echo "Complete."
+export TESTS_PATH=$(cd `dirname $0`; pwd)
+echo "TESTS_PATH=${TESTS_PATH}"
+export SCRIPTS_PATH=$(cd `dirname $0`; cd ../../osgpkitools; pwd)
+echo "SCRIPTS_PATH=${SCRIPTS_PATH}"
+export PYTHON="python"
+echo "PYTHON=${PYTHON}"
+
+shopt -s nullglob  # Returns empty string if nothing matches glob
+for test in ${TESTS_PATH}/test-*.sh ; do
+    echo "Executing test: ${test}"
+    ${test}
+    if test $? -ne 0 ; then
+        echo "Exit status: $?"
+        status=1
+    fi
+    echo "Test complete: ${test}"
+done
+
+echo "BaTLab tests complete."
 /bin/date
-exit 0
+exit ${status}
+
