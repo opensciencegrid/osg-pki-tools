62c21fa2e0927885c12469d538fb3942ca42757b
Von Welch <vwelch@indiana.edu>
2013-03-28 15:38:27 +0000

    pki-clients.ini is in /etc/osg/ not /etc/
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/tags/1.1@16515 4e558342-562e-0410-864c-e07659590f8c

--- osgpkitools/OSGPKIUtils.py
+++ osgpkitools/OSGPKIUtils.py
@@ -149,8 +149,8 @@ def CreateOIMConfig(isITB, **OIMConfig):
         Config.read(str(os.environ['HOME']) + '/.osg-pki/OSG_PKI.ini')
     elif os.path.exists('pki-clients.ini'):
         Config.read('pki-clients.ini')
-    elif os.path.exists('/etc/pki-clients.ini'):
-        Config.read('/etc/pki-clients.ini')
+    elif os.path.exists('/etc/osg/pki-clients.ini'):
+        Config.read('/etc/osg/pki-clients.ini')
     else:
         raise FileNotFoundException('pki-clients.ini',
                                     'Could not locate the file')
