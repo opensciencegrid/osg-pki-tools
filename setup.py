33c3bc562b2c03b1f5ab8d7667b401bcce6d4cfa
Viplav Khadke <vdkhadke@indiana.edu>
2013-06-25 22:54:04 +0000

    Fixing setup.py
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-cert-scripts-v3/branches/1.2.1@17084 4e558342-562e-0410-864c-e07659590f8c

--- setup.py
+++ setup.py
@@ -11,17 +11,17 @@ def read(fname):
 
 setup(
     name = "OSGPKITools",
-    version = "1.1.0",
+    version = "1.2.1",
     author = "Rohan Mathure",
     author_email = "rmathure@indiana.edu",
     description = ("A utility to request and manage certificates."),
     data_files = [('/usr/bin/',['osgpkitools/osg-cert-request', 'osgpkitools/osg-cert-retrieve', 'osgpkitools/osg-gridadmin-cert-request', 'osgpkitools/osg-cert-revoke', 'osgpkitools/osg-user-cert-revoke', 'osgpkitools/osg-user-cert-renew']),
                  ('/etc/osg/',['osgpkitools/pki-clients.ini']),
-                 (os.path.join(get_python_lib(), 'osgpkitools/'),['osgpkitools/OSGPKIUtils.py','osgpkitools/__init__.py','osgpkitools/ExceptionDefinitions.py'])],
+                 (os.path.join(get_python_lib(), 'osgpkitools/'),['osgpkitools/OSGPKIUtils.py','osgpkitools/__init__.py','osgpkitools/ExceptionDefinitions.py','osgpkitools/ConnectAPI.py'])],
     packages=['osgpkitools', 'tests'],
     long_description=read('README.txt'),
     classifiers=[
-        "Development Status :: 1 - Alpha",
+        "Development Status :: 1.2.1 - Alpha",
         "Topic :: Utilities",
         "Programming Language :: Python"
         "Operating System :: POSIX :: Linux"
