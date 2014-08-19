82736cfe1966ca764f7eba42f53dafe58b5ce125
Brian Lin <blin@cs.wisc.edu>
2014-08-19 21:52:06 +0000

    Bump version to 1.2.10
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-pki-tools/trunk@19619 4e558342-562e-0410-864c-e07659590f8c

--- osgpkitools/OSGPKIUtils.py
+++ osgpkitools/OSGPKIUtils.py
@@ -25,7 +25,7 @@ MBSTRING_ASC = MBSTRING_FLAG | 1
 MBSTRING_BMP = MBSTRING_FLAG | 2
 
 # The variable for storing version number for the scripts
-Version_Number = "1.2.9"
+Version_Number = "1.2.10"
 
 
 def get_ssl_context(**arguments):
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name = "OSGPKITools",
-    version = "1.2.9",
+    version = "1.2.10",
     author = "Rohan Mathure",
     author_email = "rmathure@indiana.edu",
     maintainer = "Brian Lin",
@@ -23,7 +23,7 @@ setup(
     packages=['osgpkitools', 'tests'],
     long_description=read('README.txt'),
     classifiers=[
-        "Development Status :: 1.2.9",
+        "Development Status :: 1.2.10",
         "Topic :: Utilities",
         "Programming Language :: Python"
         "Operating System :: POSIX :: Linux"
