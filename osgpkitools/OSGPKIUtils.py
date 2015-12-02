6e3a3e1d48e0abaf628b775fe844ceef89bb7660
Brian Lin <blin@cs.wisc.edu>
2015-12-02 19:22:07 +0000

    Branch for refactoring (SOFTWARE-2119)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-pki-tools/branches/sw2119_refactor@22169 4e558342-562e-0410-864c-e07659590f8c

--- osgpkitools/OSGPKIUtils.py
+++ osgpkitools/OSGPKIUtils.py
@@ -108,6 +108,8 @@ def check_permissions(path):
         raise FileWriteException("User does not have appropriate permissions for writing to current directory.")
 
 def find_existing_file_count(filename):
+    '''Check if filename and revisions of the filename exists. If so, increment the revision number and return
+    the latest revision filename'''
     temp_name = filename.split(".")[-2]
     trimmed_name = temp_name
     version_count = 0
@@ -126,10 +128,6 @@ def find_existing_file_count(filename):
         return new_file
     else:
         return filename
-
-#    os.rename(filename, new_file)
-#    subprocess.call(["touch", filename])
-#    print new_file
 
 def check_response_500(response):
     """ This functions handles the 500 error response from the server"""
@@ -350,6 +348,15 @@ class Cert:
 
         self.X509Request.set_subject_name(X509Name)
 
+        alt_names = config_items.get('alt_names')
+        if alt_names:
+            extension_stack = X509.X509_Extension_Stack()
+            extension = X509.new_extension('subjectAltName',
+                                           ", ".join(['DNS:%s' % name for name in alt_names]))
+            extension.set_critical(1)
+            extension_stack.push(extension)
+            self.X509Request.add_extensions(extension_stack)
+
         #
         # publickey
         #
@@ -357,5 +364,4 @@ class Cert:
         self.X509Request.set_pubkey(pkey=self.PKey)
         self.X509Request.sign(pkey=self.PKey, md='sha1')
         return self.X509Request
-
 
