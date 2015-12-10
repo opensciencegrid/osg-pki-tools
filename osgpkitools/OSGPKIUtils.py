c55f6ffa3b485f28fa57aa97397cedc7ec83e671
Brian Lin <blin@cs.wisc.edu>
2015-12-10 16:13:23 +0000

    Bump version to 1.2.13
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/osg-pki-tools/trunk@22198 4e558342-562e-0410-864c-e07659590f8c

--- osgpkitools/OSGPKIUtils.py
+++ osgpkitools/OSGPKIUtils.py
@@ -20,7 +20,7 @@ MBSTRING_ASC = MBSTRING_FLAG | 1
 MBSTRING_BMP = MBSTRING_FLAG | 2
 
 # The variable for storing version number for the scripts
-Version_Number = "1.2.12"
+Version_Number = "1.2.13"
 
 
 def get_ssl_context(**arguments):
