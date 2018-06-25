8c1ed6b291b6c7304d89266db96f666a93ff2b85
Brian Lin <brianhlin@gmail.com>
2018-06-25 17:09:57 -0500

    Complete rewrite of osg-cert-request
    - Separate osg-cert-request functions into its own library for easier testing
    - Gut utils lib, we don't need most of it anymore

---
+++ osgpkitools/request.py
@@ -0,0 +1,154 @@
+"""osg-cert-request tool module, separated from the script for unit testing
+"""
+from __future__ import print_function
+
+import os
+import re
+import sys
+import argparse
+
+from collections import namedtuple
+from M2Crypto import RSA, EVP, X509
+from osgpkitools import utils
+
+
+def parse_cli(args):
+    """This function parses all the arguments, validates them and then stores
+    them in a dictionary that is used throughout in the script."""
+
+    parser = argparse.ArgumentParser(add_help=False)  # disable built-in help to control help message ordering
+
+    required = parser.add_argument_group('Required', 'Specify only one of -H/--hostname and -F/--hostfile')
+    hosts = required.add_mutually_exclusive_group(required=True)
+    hosts.add_argument('-H', '--hostname', action='store', dest='hostname',
+                       help='The hostname (FQDN) to request')
+    hosts.add_argument('-F', '--hostfile', action='store', dest='hostfile',
+                       help='File containing list of hostnames (FQDN), one per line, to request. '
+                       'Space separated subject alternative names may be specified on the same line as each hostname.')
+    required.add_argument('-C', '--country', action=CountryAction, required=True, dest='country',
+                          help='The country to associate with the generated certificate request(s)')
+    required.add_argument('-S', '--state', action=StateAction, required=True, dest='state',
+                          help='The state/province to associate with the generated certificate request(s)')
+    required.add_argument('-L', '--locality', action='store', required=True, dest='locality',
+                          help='The locality (i.e., city, town) to associate with the generated certificate request(s)')
+    required.add_argument('-O', '--organization', action='store', required=True, dest='organization',
+                          help='The organization to associate with the generated certificate request(s)')
+
+    optional = parser.add_argument_group("Optional")
+    optional.add_argument('-h', '--help', action='help',
+                          help='show this help message and exit')
+    optional.add_argument('-a', '--altname', action='append', dest='altnames', default=[],
+                          help='Specify the Subject Alternative Name for the requested certificate (only works with '
+                          '-H/--hostname). Can be specified more than once.')
+    optional.add_argument('-d', '--directory', action='store', dest='write_directory', default='.',
+                          help="Write the generated host key to this directory")
+    optional.add_argument('-V', '--version', action='version', version=utils.VERSION_NUMBER)
+
+    return parser.parse_args(args)
+
+
+class CountryAction(argparse.Action):
+    """Action for validating state/province options
+    """
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("-C/--country only accepts a single argument")
+        super(CountryAction, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values.isalpha() and len(values) == 2:
+            setattr(namespace, self.dest, values)
+        else:
+            parser.print_usage()
+            raise ValueError("Values for -C/--country should be the two-letter country code.")
+
+
+class StateAction(argparse.Action):
+    """Action for validating state/province options
+    """
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("-S/--state only accepts a single argument")
+        super(StateAction, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values.isalpha() and len(values) > 2:
+            setattr(namespace, self.dest, values)
+        else:
+            parser.print_usage()
+            raise ValueError("Values for -S/--state should be the unabbreviated form of the state or province.")
+
+
+def write_csr(output_dir, hostnames, location):
+    """Write a Certificate Signing Request and private key
+
+    Input:
+    - output_dir: The destination directory to write the request and key
+    - hostnames: A list of hostnames, the first of which is used as the Common Name.
+    Additional hostnames are added to the list of Subject Alternative Names
+    - location: A namedtuple containing country (e.g., US), state (e.g., Wisconsin),
+    locality (e.g., Madison), and organization (e.g. Unversity of WIsconsin)
+    """
+    path = os.path.join(output_dir, hostnames[0] + '.req')
+    keypath = os.path.join(output_dir, hostnames[0] + '-key.pem')
+
+    keypair = RSA.gen_key(2048, 0x10001, lambda: None)
+    utils.safe_write(keypath, keypair.as_pem(cipher=None))
+
+    # The message digest shouldn't matter here since we don't use
+    # PKey.sign_*() or PKey.verify_*() but there's no harm in keeping it and
+    # it ensures a strong hashing algo (default is sha1) if we do decide to
+    # sign things in the future
+
+    x509request = X509.Request()
+    x509name = X509.X509_Name()
+
+    for key, val in [('C', location.country), ('ST', location.state), ('L', location.locality),
+                     ('O', location.organization), ('CN', hostnames[0])]:
+        x509name.add_entry_by_txt(field=key, type=0x1000 | 1, entry=val, len=-1, loc=-1, set=0)
+
+    x509request.set_subject_name(x509name)
+
+    extension_stack = X509.X509_Extension_Stack()
+    extension = X509.new_extension('subjectAltName', ", ".join(['DNS:%s' % name for name in hostnames]))
+    extension.set_critical(1)
+    extension_stack.push(extension)
+    x509request.add_extensions(extension_stack)
+
+    pubkey = EVP.PKey(md='sha256')
+    pubkey.assign_rsa(keypair)
+    x509request.set_pubkey(pkey=pubkey)
+    x509request.set_version(0)
+    x509request.sign(pkey=pubkey, md='sha256')
+
+    try:
+        utils.safe_write(path, x509request.as_pem())
+    except:
+        os.remove(keypath)  # if we can't write the CSR, remove its associated privkey
+        raise
+
+
+def main():
+    """The entrypoint for osg-cert-request
+    """
+    try:
+        args = parse_cli(sys.argv[1:])
+    except ValueError as exc:
+        raise SystemExit(exc.message)
+
+    if args.hostfile and args.altnames:
+        print("-A/--altname option ignored with -F/--hostfile", file=sys.stderr)
+
+    location = namedtuple('Location', ['country', 'state', 'locality', 'organization'])
+    loc = location(args.country, args.state, args.locality, args.organization)
+
+    if args.hostname:
+        fqdns_list = [[args.hostname] + args.altnames]
+    elif args.hostfile:
+        with open(args.hostfile, 'r') as hfile:
+            hostfile = hfile.readlines()
+        fqdns_list = [re.split(r' +', x) for x in hostfile]
+
+    for fqdns in fqdns_list:
+        print("Writing CSR for {0}...".format(fqdns[0]))
+        write_csr(os.path.abspath(args.write_directory), fqdns, loc)
--- tests/test_cert_request.py
+++ tests/test_cert_request.py
@@ -1,117 +1,147 @@
 """Test cert-request script"""
 
 import re
+import sys
 import unittest
-from pkiunittest import OIM, DOMAIN, test_env_setup, test_env_teardown
+
+from itertools import chain, permutations
+from contextlib import contextmanager
+from StringIO import StringIO
+
+from osgpkitools import request
+
+HOST = 'hostname.example.edu'
+HOST_ARGS = [('--hostname', HOST)]
+HOST_FILE = 'hosts.txt'
+HOSTFILE_ARGS = [('--hostfile', HOST_FILE)]
+LOCATION_ARGS = [('--country', 'US'),
+                 ('--state', 'Wisconsin'),
+                 ('--locality', 'Madison'),
+                 ('--organization', 'University of Wisconsin - Madison')]
+
+
+# Capture stdout/stderr from argparse from stack overflow:
+# https://stackoverflow.com/questions/18651705/argparse-unit-tests-suppress-the-help-message
+@contextmanager
+def capture_sys_output():
+    capture_out, capture_err = StringIO(), StringIO()
+    current_out, current_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = capture_out, capture_err
+        yield capture_out, capture_err
+    finally:
+        sys.stdout, sys.stderr = current_out, current_err
+
+
+def intersperse(lst, item):
+    """Intersperse 'lst' with 'item'
+
+    >>> intersperse([1, 2, 3], 0)
+    [0, 1, 0, 2, 0, 3]
+    """
+    result = [item] * (len(lst) * 2)
+    result[1::2] = lst
+    return result
+
+
+def parse_cli(args):
+    """Parse CLI args, flattening any tuples
+    """
+    if len(args) > 1:
+        args = chain.from_iterable(args)
+    request.parse_cli(args)
+
 
 class CertRequestTests(unittest.TestCase):
 
-    def setUp(self):
-        """Run each test in its own dir"""
-        test_env_setup()
+    # def assertOption(self, sopt, lopt, dest, val):
+    #     """Ensure that the hostfile is stored
+    #     """
+    #     try:
+    #         parser = request.parse_cli([sopt, val])
+    #     except SystemExit:
+    #         pass
 
-    def tearDown(self):
-        """Remove personal test dir"""
-        test_env_teardown()
+    #     try:
+    #         lparser = request.parse_cli([lopt, val])
+    #     except SystemExit:
+    #         pass
 
-    def test_help(self):
-        """Verify help message"""
-        for option in ('-h', '--help'):
-            return_code, stdout, _, msg = OIM().request(option)
-            self.assertEqual(return_code, 0, "Bad return code when requesting help\n%s" % msg)
-            self.assert_(re.search(r'[Uu]sage:', stdout), msg)
+    #     self.assertEqual(getattr(parser, dest), val, "{0} did not store {1} in the expected destination, {2}"
+    #                      .format(sopt, val, dest))
+    #     self.assertEqual(parser, lparser, "{0} and {1} are not the same".format(sopt, lopt))
+
+    def test_conflicting_opts(self):
+        """Users should not provide both a hostname and hosts file
+        """
+        with self.assertRaises(SystemExit) as cm:
+            with capture_sys_output() as (stdout, stderr):
+                parse_cli(HOST_ARGS + HOSTFILE_ARGS)
+        self.assertEqual(cm.exception.code, 2, 'conflicting options did not exit 2')
 
     def test_required_opts(self):
-        """Test required options"""
-        # missing CSR/hostname
-        return_code, _, _, msg = OIM().request('--phone', '1234567', '--email', 'foo@bar.com', '--name',
-                                               'Foo Barrington')
-        self.assertEqual(return_code, 2, "Missing CSR and hostname options did not fail\n%s" % msg)
-
-        # missing phone number
-        return_code, _, _, msg = OIM().request('--email', 'foo@bar.com', '--name', 'Foo Barrington', '--csr', 'csr_file')
-        self.assertEqual(return_code, 2, "Missing phone option did not fail\n%s" % msg)
-
-        # missing name
-        return_code, _, _, msg = OIM().request('--phone', '1234567', '--email', 'foo@bar.com', '--csr', 'csr_file')
-        self.assertEqual(return_code, 2, "Missing name option did not fail\n%s" % msg)
-
-        # missing email
-        return_code, _, _, msg = OIM().request('--phone', '1234567', '--name', 'Foo Barrington', '--csr', 'csr_file')
-        self.assertEqual(return_code, 2, "Missing email option did not fail\n%s" % msg)
-
-    def test_phone_validation(self):
-        """Test valid phone numbers (r'[0-9-]*')
+        """Users need to provide location information and hostname or hosts file
         """
-        return_code, _, _, msg = OIM().request('--phone', '1234567')
-        # rc should be 2 because we're missing required opts
-        self.assertEqual(return_code, 2, "Did not accept only numbers\n%s" % msg)
+        for host in [[()], HOST_ARGS, HOSTFILE_ARGS]:
+            for location in permutations(LOCATION_ARGS, 3):
+                args = host + list(location)
+                with self.assertRaises(SystemExit) as cm:
+                    with capture_sys_output() as (stdout, stderr):
+                        parse_cli(args)
 
-        return_code, _, _, msg = OIM().request('--phone', '123-4567')
-        # rc should be 2 because we're missing required opts
-        self.assertEqual(return_code, 2, "Did not accept '-'\n%s" % msg)
+                self.assertEqual(cm.exception.code, 2, "missing required options did not exit 2:\n{0}"
+                                 .format(args))
+                self.assert_(re.search(r'error.*is required.*', stderr.getvalue()))
 
-        return_code, _, _, msg = OIM().request('--phone', "what's a phone number?")
-        self.assertEqual(return_code, 2, "Invalid chars in phone number did not fail\n%s" % msg)
+    # def test_hostname_opt(self):
+    #     """Ensure that the hostname is stored
+    #     """
+    #     self.assertOption('-H', '--hostname', 'hostname', HOST)
 
-    def test_timeout_validation(self):
-        """Test valid timeouts (>0)
+    # def test_hostfile_opt(self):
+    #     """Ensure that the hostfile is stored
+    #     """
+    #     hfile = 'hosts.file'
+    #     parser = request.parse_cli(['-F', hfile])
+    #     lparser = request.parse_cli(['--hostfile', hfile])
+
+    #     self.assertEqual(parser.hostfile, hfile, "-F did not store {0} to the expected destination, 'hostname'"
+    #                      .format(hfile))
+    #     self.assertEqual(parser, lparser, "-F and --hostfile are not the same")
+
+    # def test_state_opt(self):
+    #     self.fail
+
+    # def test_country_opt(self):
+    #     self.fail
+
+    # def test_locality_opt(self):
+    #     self.fail
+
+    # def test_org_opt(self):
+    #     self.fail
+
+    def test_help_opt(self):
+        """Verify help option
         """
-        return_code, _, _, msg = OIM().request('--timeout', '10')
-        # rc should be 2 because we're missing required opts
-        self.assertEqual(return_code, 2, "Did not accept timeout > 0\n%s" % msg)
+        for opt in ['-h', '--help']:
+            with self.assertRaises(SystemExit) as cm:
+                with capture_sys_output() as (stdout, stderr):
+                    parse_cli([opt])
+            self.assertEqual(cm.exception.code, 0, '{0} did not exit 0'.format(opt))
 
-        return_code, _, _, msg = OIM().request('--timeout', '0')
-        # rc should be 2 because we're missing required opts
-        self.assertEqual(return_code, 2, "Did not fail for timeout=0\n%s" % msg)
+    # def test_san_opt(self):
+    #     """Ensure that SANs are stored as a list
+    #     """
+    #     sans = ['san1.example.edu', 'san2.example.edu', 'san3.example.edu']
+    #     parser = request.parse_cli(HOST_ARGS + intersperse(sans, '-a'))
+    #     self.assertListEqual(parser.altnames, sans, 'multiple SANs not stored as a list\n' +
+    #                          'Arguments: {0}'.format(' '.join(sans)))
 
-        return_code, _, _, msg = OIM().request('--timeout', '-5')
-        # rc should be 2 because we're missing required opts
-        self.assertEqual(return_code, 2, "Did not fail for timeout < 0\n%s" % msg)
+    #     lparser = request.parse_cli(HOST_ARGS + intersperse(sans, '--altname'))
+    #     self.assertListEqual(lparser.altnames, sans, 'multiple SANs not stored as a list\n' +
+    #                          'Arguments: {0}'.format(' '.join(sans)))
 
-        return_code, _, _, msg = OIM().request('--timeout', 'timeout')
-        self.assertEqual(return_code, 2, "Did not fail on non-number\n%s" % msg)
-
-    def test_arg_conflict(self):
-        """Verify --csr and --hostname conflict"""
-        oim = OIM()
-        return_code, _, _, msg = oim.request('--hostname', 'test.' + DOMAIN, '--csr', 'foo')
-        self.assertEqual(return_code, 2, "CSR and hostname options do not conflict\n%s" % msg)
-
-    def test_cert_request(self):
-        """Test making a request for a single host"""
-        oim = OIM()
-        return_code, _, _, msg = oim.request('--phone', '1234567', '--email', 'foo@bar.com', '--name',
-                                             'Foo Barrington', '--hostname', 'test.' + DOMAIN)
-        self.assertEqual(return_code, 0, "Failed to request certificate\n%s" % msg)
-        self.assert_(oim.reqid != '', msg)
-
-    def test_unauthenticated(self):
-        """Test unauthenticated request"""
-        oim = OIM()
-        return_code, _, _, msg = oim.request('--phone', '1234567', '--email', 'foo@bar.com', '--name',
-                                             'Foo Barrington', '--hostname', 'test.' + DOMAIN, '--unauthenticated')
-        self.assertEqual(return_code, 0, "Failed to perform unauthenticated request\n%s" % msg)
-        self.assert_(oim.reqid != '', msg)
-
-    def test_service_cert(self):
-        """Test making a request for a service cert"""
-        oim = OIM()
-        return_code, _, _, msg = oim.request('--phone', '1234567', '--email', 'foo@bar.com', '--name',
-                                             'Foo Barrington', '--hostname', 'service/test.' + DOMAIN)
-        self.assertEqual(return_code, 0, "Failed to request certificate\n%s" % msg)
-        self.assert_(oim.reqid != '', msg)
-
-    def test_alt_name_request(self):
-        """Test cert request with alternative name"""
-        oim = OIM()
-        hostname = 'test.' + DOMAIN
-        san = 'test-san.' + DOMAIN
-        san2 = 'test-san2.' + DOMAIN
-        return_code, _, _, msg = oim.request('--phone', '1234567', '--email', 'foo@bar.com', '--name', 'Foo Barrington',
-                                             '--hostname', hostname, '--altname', san, '--altname', san2)
-        self.assertEqual(return_code, 0, "Failed to request certificate\n%s" % msg)
-        self.assert_(oim.reqid != '', msg)
 
 if __name__ == '__main__':
     unittest.main()
