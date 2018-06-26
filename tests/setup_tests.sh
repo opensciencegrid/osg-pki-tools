ccf6bd453ca244bb9daf2627857293fda0a3b247
Brian Lin <brianhlin@gmail.com>
2018-06-26 15:58:48 -0500

    Improve readability of setup script. Remove privileged/cgroup

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -8,18 +8,27 @@ el_version=$1
  # Run tests in Container
 if [ "$el_version" = "6" ]; then
 
-sudo docker run --rm=true -v `pwd`:/osg-pki-tools:rw centos:centos${OS_VERSION} /bin/bash -c "bash -xe /osg-pki-tools/tests/test_inside_docker.sh ${OS_VERSION}"
+    sudo docker run --rm=true \
+         --volume `pwd`:/osg-pki-tools:rw \
+         centos:centos${OS_VERSION} \
+         /bin/bash -c "bash -xe /osg-pki-tools/tests/test_inside_docker.sh ${OS_VERSION}"
 
 elif [ "$el_version" = "7" ]; then
 
-docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/osg-pki-tools:rw  centos:centos${OS_VERSION}   /usr/sbin/init
-DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
-docker logs $DOCKER_CONTAINER_ID
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /osg-pki-tools/tests/test_inside_docker.sh ${OS_VERSION};
-  echo -ne \"------\nEND OSG-PKI-TOOLS TESTS\n\";"
-docker ps -a
-docker stop $DOCKER_CONTAINER_ID
-docker rm -v $DOCKER_CONTAINER_ID
+    docker run --detach --tty --interactive --env "container=docker" \
+           --volume `pwd`:/osg-pki-tools:rw \
+           centos:centos${OS_VERSION} \
+           /usr/sbin/init
+
+    DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
+    docker logs $DOCKER_CONTAINER_ID
+    docker exec --tty --interactive $DOCKER_CONTAINER_ID \
+           /bin/bash -xec "bash -xe /osg-pki-tools/tests/test_inside_docker.sh ${OS_VERSION};
+           echo -ne \"------\nEND OSG-PKI-TOOLS TESTS\n\";"
+
+    docker ps -a
+    docker stop $DOCKER_CONTAINER_ID
+    docker rm -v $DOCKER_CONTAINER_ID
 
 fi
 
