72c49acea6c03fecadb0b3d6fa3f5996b1726eaa
Brian Lin <brianhlin@gmail.com>
2018-06-26 14:59:42 -0500

    Add rpmbuild to the test process

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -14,12 +14,37 @@ rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}
 # Broken mirror?
 echo "exclude=mirror.beyondhosting.net" >> /etc/yum/pluginconf.d/fastestmirror.conf
 
-PKG_REQS='yum-plugin-priorities m2crypto'
+PKG_REQS='yum-plugin-priorities rpm-build git gzip'
 if [ $OS_VERSION -eq '6' ]; then
-    PKG_REQS="$PKG_REQS python-unittest2 python-argparse"
+    PKG_REQS="$PKG_REQS python-unittest2"
 fi
 
 yum -y install $PKG_REQS
+
+# Prepare the RPM environment
+mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+cat >> /etc/rpm/macros.dist << EOF
+%dist .osg.el${OS_VERSION}
+%osg 1
+EOF
+
+cp osg-pki-tools/rpm/osg-pki-tools.spec /tmp/rpmbuild/SPECS
+package_version=`grep Version osg-pki-tools/rpm/osg-pki-tools.spec | awk '{print $2}'`
+pushd osg-pki-tools
+git archive --format=tar --prefix=osg-pki-tools-${package_version}/ HEAD | \
+    gzip > /tmp/rpmbuild/SOURCES/osg-pki-tools-${package_version}.tar.gz
+popd
+
+# Build the RPM
+rpmbuild --define '_topdir /tmp/rpmbuild' -ba /tmp/rpmbuild/SPECS/osg-pki-tools.spec
+
+# After building the RPM, try to install it
+# Fix the lock file error on EL7.  /var/lock is a symlink to /var/run/lock
+mkdir -p /var/run/lock
+
+RPM_LOCATION=
+yum localinstall -y /tmp/rpmbuild/RPMS/noarch/osg-pki-tools-${package_version}*
 
 # Run unit tests
 pushd osg-pki-tools/
