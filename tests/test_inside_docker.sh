5baf43901d42e18f8a413ea4194ac9728eaff45b
Brian Lin <brianhlin@gmail.com>
2018-06-26 16:30:09 -0500

    Add argparse requirement

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -14,7 +14,12 @@ rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}
 # Broken mirror?
 echo "exclude=mirror.beyondhosting.net" >> /etc/yum/pluginconf.d/fastestmirror.conf
 
-yum -y install yum-plugin-priorities m2crypto python-unittest2
+PKG_REQS='yum-plugin-priorities m2crypto'
+if [ $OS_VERSION -eq '6' ]; then
+    PKG_REQS="$PKG_REQS python-unittest2 python-argparse"
+fi
+
+yum -y install $PKG_REQS
 
 # Run unit tests
 pushd osg-pki-tools/
