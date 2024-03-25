8e52306f85582ea626aa3e745547542d35ddae53
Pat Riehecky <riehecky@fnal.gov>
2024-03-25 17:11:56 -0500

    Mass format from pre-commit

--- tests/OSGPKIUtilsTests.py
+++ tests/OSGPKIUtilsTests.py
@@ -2,54 +2,68 @@
 
 import re
 import signal
-from json import dumps
 import sys
 import unittest
 
-from .pkiunittest import DOMAIN, EMAIL, PYPATH
+from json import dumps
 
 # Allow import of OSGPKIUtilsTests. Hacky.
 sys.path.insert(1, PYPATH)
+
 from osgpkitools import OSGPKIUtils
 
+from .pkiunittest import DOMAIN, EMAIL, PYPATH
+
+
 class OSGPKIUtilsTests(unittest.TestCase):
-    FQDN = 'test.' + DOMAIN
+    FQDN = "test." + DOMAIN
 
     def test_csr_generation(self):
-        '''Generate a basic CSR'''
+        """Generate a basic CSR"""
         cert = OSGPKIUtils.Cert(self.FQDN, email=EMAIL)
-        self.assertTrue(cert.x509request, 'missing CSR contents')
+        self.assertTrue(cert.x509request, "missing CSR contents")
 
     def test_alt_name_csr_generation(self):
-        '''Generate a CSR with multiple SANs'''
-        alias = 'test-san.' + DOMAIN
+        """Generate a CSR with multiple SANs"""
+        alias = "test-san." + DOMAIN
         cert = OSGPKIUtils.Cert(self.FQDN, altnames=[alias], email=EMAIL)
         csr_contents = cert.x509request.as_text()
-        self.assertIsNotNone(re.search(r'X509v3 Subject Alternative Name: critical', csr_contents),
-                     "Subject Alternative Name not marked as 'critical'\n" + csr_contents)
-        found_names = set(match.group(1) for match in re.finditer(r'DNS:([\w\-\.]+)', csr_contents))
-        expected_names = set([alias])
-        self.assertEqual(found_names, expected_names,
-                         f"Did not find expected SAN contents {list(expected_names)}:\n{csr_contents}") # printed lists are easier to read` than printed sets
+        self.assertIsNotNone(
+            re.search(r"X509v3 Subject Alternative Name: critical", csr_contents),
+            "Subject Alternative Name not marked as 'critical'\n" + csr_contents,
+        )
+        found_names = {match.group(1) for match in re.finditer(r"DNS:([\w\-\.]+)", csr_contents)}
+        expected_names = {alias}
+        self.assertEqual(
+            found_names, expected_names, f"Did not find expected SAN contents {list(expected_names)}:\n{csr_contents}"
+        )  # printed lists are easier to read` than printed sets
 
     def test_timeout(self):
-        '''Verify timeout length'''
-        OSGPKIUtils.start_timeout_clock(1) # 1 minute timeout
-        alarm_timer = signal.alarm(0) # cancel alarm and get remaining time of previous alarm
-        self.assertEqual(alarm_timer, 60, f'Expected 1 min timeout, got {alarm_timer}s')
+        """Verify timeout length"""
+        OSGPKIUtils.start_timeout_clock(1)  # 1 minute timeout
+        alarm_timer = signal.alarm(0)  # cancel alarm and get remaining time of previous alarm
+        self.assertEqual(alarm_timer, 60, f"Expected 1 min timeout, got {alarm_timer}s")
 
     def test_sigalrm_handler(self):
-        '''Verify exiting handler'''
+        """Verify exiting handler"""
         self.assertRaises(SystemExit, OSGPKIUtils.start_timeout_clock, 0)
 
     def test_missing_vo_exception(self):
-        '''Verify helpful error message when user fails to include necessary VO information.'''
-        response = dumps({'status': 'FAILED',
-                          'detail': ' -- '.join(["Failed to find GridAdmins for specified CSRs/VO",
-                                                 "Couldn't find GridAdmin group under specified VO.",
-                                                 "GOC alert will be sent to GOC infrastructure team about this " + \
-                                                 "issue. Meanwhile, feel free to open a GOC ticket at " + \
-                                                 "https://ticket.grid.iu.edu"])})
+        """Verify helpful error message when user fails to include necessary VO information."""
+        response = dumps(
+            {
+                "status": "FAILED",
+                "detail": " -- ".join(
+                    [
+                        "Failed to find GridAdmins for specified CSRs/VO",
+                        "Couldn't find GridAdmin group under specified VO.",
+                        "GOC alert will be sent to GOC infrastructure team about this "
+                        + "issue. Meanwhile, feel free to open a GOC ticket at "
+                        + "https://ticket.grid.iu.edu",
+                    ]
+                ),
+            }
+        )
         try:
             OSGPKIUtils.print_failure_reason_exit(response)
         except SystemExit:
@@ -58,12 +72,13 @@ class OSGPKIUtilsTests(unittest.TestCase):
             self.fail("print_failure_reason_exit() did not raise SystemExit")
 
     def test_read_config(self):
-        '''Verify that configuration is read in as a dictionary'''
-        for section, itb in list({'production': False, 'ITB': True}.items()):
-            self.assertTrue(OSGPKIUtils.read_config(itb, config_files=['../osgpkitools/pki-clients.ini']),
-                         f'Unable to read the {section} config section')
+        """Verify that configuration is read in as a dictionary"""
+        for section, itb in list({"production": False, "ITB": True}.items()):
+            self.assertTrue(
+                OSGPKIUtils.read_config(itb, config_files=["../osgpkitools/pki-clients.ini"]),
+                f"Unable to read the {section} config section",
+            )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/pkiunittest.py
+++ tests/pkiunittest.py
@@ -11,7 +11,8 @@ import sys
 import tempfile
 
 from copy import deepcopy
-from subprocess import Popen, PIPE
+from subprocess import PIPE, Popen
+
 from M2Crypto import RSA, X509
 
 global TEST_PATH
@@ -42,10 +43,11 @@ SCRIPTS_PATH = os.path.abspath("../osgpkitools")
 # Scripts import from osgpkitools, and it is up a directory
 PYPATH = os.path.abspath("..")
 
-TEST_PATH = ''
+TEST_PATH = ""
 
 # The expected subject for CILogon host/service certs before the common name
-HOST_SUBJECT_PREFIX = '/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN='
+HOST_SUBJECT_PREFIX = "/DC=org/DC=opensciencegrid/O=Open Science Grid/OU=Services/CN="
+
 
 def test_env_setup():
     """Create a test dir and environment"""
@@ -56,66 +58,74 @@ def test_env_setup():
     # Set path and python path for tests
     orig_env = deepcopy(os.environ)
     try:
-        os.environ['PYTHONPATH'] += f':{PYPATH}'
+        os.environ["PYTHONPATH"] += f":{PYPATH}"
     except KeyError:
-        os.environ['PYTHONPATH'] = PYPATH
-    os.environ['PATH'] += f':{SCRIPTS_PATH}'
+        os.environ["PYTHONPATH"] = PYPATH
+    os.environ["PATH"] += f":{SCRIPTS_PATH}"
 
     # Create temp dir and place necessary config in the cwd
-    ini_file = os.path.join(SCRIPTS_PATH, 'pki-clients.ini')
+    ini_file = os.path.join(SCRIPTS_PATH, "pki-clients.ini")
     cwd = os.getcwd()
     shutil.copy2(ini_file, cwd)
     TEST_PATH = tempfile.mkdtemp(dir=cwd)
 
+
 def test_env_teardown():
     """Blow up the test dir"""
-    os.environ = deepcopy(orig_env) # restore environment
-    os.unlink('pki-clients.ini')
+    os.environ = deepcopy(orig_env)  # restore environment
+    os.unlink("pki-clients.ini")
     shutil.rmtree(TEST_PATH)
+
 
 def run_command(cmd, env=None):
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE, env=env)
     stdout, stderr = proc.communicate()
     rc = proc.returncode
-    diagnostic = f"Command: {' '.join(cmd)}\n"  \
-                 + f"Return code: {rc}\n"  \
-                 + "STDOUT:\n" + stdout \
-                 + "STDERR:\n" + stderr
+    diagnostic = f"Command: {' '.join(cmd)}\n" + f"Return code: {rc}\n" + "STDOUT:\n" + stdout + "STDERR:\n" + stderr
     return rc, stdout, stderr, diagnostic
 
+
 def run_python(script, *args):
-    '''Run osg-pki-tools script '''
+    """Run osg-pki-tools script"""
     script_path = os.path.join(SCRIPTS_PATH, script)
     py_cmd = (sys.executable, script_path) + args
     return run_command(py_cmd, env=os.environ)
 
-class OIM(object):
+
+class OIM:
     """OIM and cert/key pair interface"""
+
     def __init__(self):
         """Create a cert instance"""
-        self.reqid = ''
+        self.reqid = ""
         self.certs = list()
         self.keys = list()
 
     def request(self, *opts):
         """Run osg-cert-request"""
-        rc, stdout, stderr, msg = run_python('osg-cert-request',
-                                             '--test',
-                                             '--comment', 'osg-pki-tools developer testing',
-                                             '--cc', 'test@example.com,test2@example.com',
-                                             '--directory', TEST_PATH,
-                                             '--vo', TEST_VO,
-                                             *opts)
+        rc, stdout, stderr, msg = run_python(
+            "osg-cert-request",
+            "--test",
+            "--comment",
+            "osg-pki-tools developer testing",
+            "--cc",
+            "test@example.com,test2@example.com",
+            "--directory",
+            TEST_PATH,
+            "--vo",
+            TEST_VO,
+            *opts,
+        )
 
         try:
-            self.reqid = re.search(r'OIM Request ID: (\d+)', stdout).group(1)
+            self.reqid = re.search(r"OIM Request ID: (\d+)", stdout).group(1)
         except AttributeError:
-            msg = 'Could not parse stdout for key or request ID\n' + msg
+            msg = "Could not parse stdout for key or request ID\n" + msg
 
         try:
-            key_path = re.search(r'Writing key to ([^\n]+)', stdout).group(1)
+            key_path = re.search(r"Writing key to ([^\n]+)", stdout).group(1)
         except AttributeError:
-            msg = 'Could not parse stdout for key or request ID\n' + msg
+            msg = "Could not parse stdout for key or request ID\n" + msg
         else:
             try:
                 key = OIM.verify_key(key_path)
@@ -126,28 +136,34 @@ class OIM(object):
 
     def gridadmin_request(self, *opts):
         """Run osg-gridadmin-request"""
-        rc, stdout, stderr, msg = run_python('osg-gridadmin-cert-request',
-                                             '--test',
-                                             '--cert', GA_CERT_PATH,
-                                             '--pkey', GA_KEY_PATH,
-                                             '--directory', TEST_PATH,
-                                             '--vo', TEST_VO,
-                                             *opts)
+        rc, stdout, stderr, msg = run_python(
+            "osg-gridadmin-cert-request",
+            "--test",
+            "--cert",
+            GA_CERT_PATH,
+            "--pkey",
+            GA_KEY_PATH,
+            "--directory",
+            TEST_PATH,
+            "--vo",
+            TEST_VO,
+            *opts,
+        )
         # Populate instance attr
         try:
-            self.reqid = re.search(r'OIM Request ID: (\d+)', stdout).group(1)
+            self.reqid = re.search(r"OIM Request ID: (\d+)", stdout).group(1)
         except AttributeError:
-            msg = 'Could not parse stdout for request ID\n' + msg
+            msg = "Could not parse stdout for request ID\n" + msg
         # find all certs and keys in the output dir as sorted lists
-        certs = sorted([x for x in glob.glob(os.path.join(TEST_PATH, '*.pem')) if '-key.pem' not in x])
-        keys = sorted(glob.glob(os.path.join(TEST_PATH, '*-key.pem')))
+        certs = sorted([x for x in glob.glob(os.path.join(TEST_PATH, "*.pem")) if "-key.pem" not in x])
+        keys = sorted(glob.glob(os.path.join(TEST_PATH, "*-key.pem")))
         if len(certs) != len(keys):
-            raise AssertionError('Mismatched number of issued certs and keys\n' + msg)
+            raise AssertionError("Mismatched number of issued certs and keys\n" + msg)
 
         # Verify permissions of created files, if any
         for cert_path, key_path in zip(certs, keys):
             try:
-                subject = HOST_SUBJECT_PREFIX + os.path.basename(os.path.splitext(cert_path)[0]).replace('_', '/')
+                subject = HOST_SUBJECT_PREFIX + os.path.basename(os.path.splitext(cert_path)[0]).replace("_", "/")
                 cert = OIM.verify_cert(cert_path, subject)
                 key = OIM.verify_key(key_path)
             except CertFileError as cert_err:
@@ -161,57 +177,60 @@ class OIM(object):
 
     def retrieve(self, *opts):
         """Run osg-cert-retrieve"""
-        if not self.reqid and '--help' not in opts:
-            raise CertFileError('Could not revoke cert due to missing request ID\n')
+        if not self.reqid and "--help" not in opts:
+            raise CertFileError("Could not revoke cert due to missing request ID\n")
         args = list(opts + (self.reqid,))
-        return run_python('osg-cert-retrieve', '--test',
-                          '--directory', TEST_PATH,
-                          *args)
+        return run_python("osg-cert-retrieve", "--test", "--directory", TEST_PATH, *args)
 
     def user_renew(self, *opts):
         """Run osg-user-cert-renew"""
-        return run_python('osg-user-cert-renew', '--test', *opts)
+        return run_python("osg-user-cert-renew", "--test", *opts)
 
     def revoke(self, *opts):
         """Run osg-cert-revoke"""
-        if not self.reqid and '--help' not in opts:
-            raise CertFileError('Could not revoke cert due to missing request ID\n')
-        args = opts + ('--test',
-                       '--cert', GA_CERT_PATH,
-                       '--pkey', GA_KEY_PATH,
-                       self.reqid, 'osg-pki-tools unit test - revoke')
-        return run_python('osg-cert-revoke', *args)
+        if not self.reqid and "--help" not in opts:
+            raise CertFileError("Could not revoke cert due to missing request ID\n")
+        args = opts + (
+            "--test",
+            "--cert",
+            GA_CERT_PATH,
+            "--pkey",
+            GA_KEY_PATH,
+            self.reqid,
+            "osg-pki-tools unit test - revoke",
+        )
+        return run_python("osg-cert-revoke", *args)
 
     def user_revoke(self, *opts):
         """Run osg-user-cert-revoke, which is a bash wrapper around osg-cert-revoke"""
-        if not self.reqid and '--help' not in opts:
-            raise CertFileError('Could not revoke cert due to missing request ID\n')
-        args = opts + ('--test', self.reqid, 'osg-pki-tools unit test - user revoke')
-        cmd = (os.path.join(SCRIPTS_PATH, 'osg-user-cert-revoke'),) + args
+        if not self.reqid and "--help" not in opts:
+            raise CertFileError("Could not revoke cert due to missing request ID\n")
+        args = opts + ("--test", self.reqid, "osg-pki-tools unit test - user revoke")
+        cmd = (os.path.join(SCRIPTS_PATH, "osg-user-cert-revoke"),) + args
         return run_command(cmd)
 
     @staticmethod
     def verify_key(path):
         """Ensure proper key permission bits and ability to unlock the key"""
-        fail_prefix = 'VerificationFailure: '
+        fail_prefix = "VerificationFailure: "
         if not os.path.exists(path):
             raise KeyFileError(fail_prefix + "No associated key file\n")
-        permissions = os.stat(path).st_mode & 0o777 # Mask non-permission bits
+        permissions = os.stat(path).st_mode & 0o777  # Mask non-permission bits
         if permissions != 0o600:
             raise KeyFileError(fail_prefix + f"Bad permissions ({permissions}) on key '{path}'\n")
         try:
             key = RSA.load_key(path, OIM.simple_pass_callback)
         except RSA.RSAError as exc:
-            if 'no start line' in exc:
+            if "no start line" in exc:
                 raise KeyFileError(fail_prefix + f"Could not load key file '{path}'\n")
-            elif 'bad pass' in exc:
+            elif "bad pass" in exc:
                 raise KeyFileError(fail_prefix + f"Incorrect pass for key file {path}\n")
         return key
 
     @staticmethod
     def verify_cert(path, expected_subject):
         """Ensure proper cert permissions, returns"""
-        fail_prefix = 'VerificationFailure: '
+        fail_prefix = "VerificationFailure: "
         if not os.path.exists(path):
             raise CertFileError(fail_prefix + "No associated cert file\n")
         mode = os.stat(path).st_mode
@@ -221,21 +240,21 @@ class OIM(object):
             cert = X509.load_cert(path)
             cert_subject = str(cert.get_subject())
         except X509.X509Error:
-            raise CertFileError(f'Malformed cert: {path}\n')
+            raise CertFileError(f"Malformed cert: {path}\n")
         if cert_subject != expected_subject:
-            raise CertFileError(f'Incorrect subject. EXPECTED:\n{expected_subject}\nGOT:\n{cert_subject}\n')
+            raise CertFileError(f"Incorrect subject. EXPECTED:\n{expected_subject}\nGOT:\n{cert_subject}\n")
         return cert
 
     @staticmethod
     def simple_pass_callback(verify):
         """Callback for unlocking keys with passwords in plaintext for testing."""
-        return ''
+        return ""
 
     def assertNumCerts(self, num_expected_certs, msg):
         """Verify expected number of certs"""
         num_found_certs = len(self.certs)
         if num_found_certs != num_expected_certs:
-            raise AssertionError(f'Expected {num_found_certs} cert(s), found {num_expected_certs}\n{msg}')
+            raise AssertionError(f"Expected {num_found_certs} cert(s), found {num_expected_certs}\n{msg}")
 
     def assertSans(self, hosts_list, msg):
         """Verify that the we have the correct number of certs and expected SAN contents for each cert.
@@ -246,14 +265,15 @@ class OIM(object):
         self.assertNumCerts(len(hosts_list), msg)
         for cert, expected_names in zip(self.certs, hosts_list):
             # Verify list of SANs are as expected
-            san_contents = cert.get_ext('subjectAltName').get_value()
-            found_names = set(match.group(1) for match in re.finditer(r'DNS:([\w\-\.]+)', san_contents))
+            san_contents = cert.get_ext("subjectAltName").get_value()
+            found_names = {match.group(1) for match in re.finditer(r"DNS:([\w\-\.]+)", san_contents)}
             if found_names != set(expected_names):
                 raise AssertionError(f"Did not find expected SAN contents {expected_names}:\n{cert.as_text()}\n{msg}")
 
 
 class KeyFileError(AssertionError):
     pass
+
 
 class CertFileError(AssertionError):
     pass
